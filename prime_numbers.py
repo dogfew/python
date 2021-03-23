9d2b9faf46f8538bd2c8c009755788af06fe8d7c
dogfew <79280805+dogfew@users.noreply.github.com>
2021-03-23 20:25:55 +0300

    Add files via upload
    My algorithm for quickly finding an even number of primes

---
+++ prime_numbers.py
@@ -0,0 +1,33 @@
+import math
+N = int(input())
+prime_numbers = [2, 3]
+i = 0
+n = 0
+while i < N - 2:
+    prime_1 = True
+    prime_2 = True
+    k, m = n + 7, n + 5
+    n = n + 6
+    sqrt = int(math.sqrt(m)) + 1
+    for j in prime_numbers:
+        if k % j == 0:
+            prime_1 = False
+            break
+        elif j > sqrt:
+            break
+    for j in prime_numbers:
+        if m % j == 0:
+            prime_2 = False
+            break
+        elif j > sqrt:
+            break
+    if prime_2:
+        prime_numbers.append(m)
+        i = i + 1
+    if prime_1:
+        prime_numbers.append(k)
+        i = i + 1
+print(prime_numbers)
+
+
+
