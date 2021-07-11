fc6314b03b9a73b5cb04c61c3a6eb35fdf910850
Vladimir Perepelkin <79280805+dogfew@users.noreply.github.com>
2021-07-12 01:50:08 +0300

    Update trying_matrix.py

--- trying_matrix.py
+++ trying_matrix.py
@@ -8,7 +8,7 @@ class Matrix:
             return [A[i]] + [c(A, i+1)]
         else:
             return A[i]
-    def mp(A, l = 1):
+    def multiply(A, l = 1):
         A = A.matrix
         B = []
         for i in range(len(A)):
@@ -17,8 +17,15 @@ class Matrix:
                 B[i].append(l * A[i][j])
         return B
     def transpose_rec(A, i=0):
+        A = A.matrix
         j = list(map(lambda x: x[i], A))
         try:
             return [j] + transpose_rec(A, i+1)
+        except:
+            return [j]
+    def multiplication(A, B, i=0):
+        j = list(map(lambda x, y: x * y, A[i], B[i]))
+        try:
+            return [j] + multiplication(A, B, i+1)
         except:
             return [j]
