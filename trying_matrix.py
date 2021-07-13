df737842817f72581a5b257805b4b8a242f43113
Vladimir Perepelkin <79280805+dogfew@users.noreply.github.com>
2021-07-13 21:24:44 +0300

    Update trying_matrix.py

--- trying_matrix.py
+++ trying_matrix.py
@@ -1,31 +1,38 @@
 class Matrix:
     def __init__(self, matrix):
         self.matrix = matrix
-    def s(self):
-        return sum(sum(i) for i in self.matrix)
-    def copy(A, i=0):
-        if i < len(A) - 1:
-            return [A[i]] + [c(A, i+1)]
-        else:
-            return A[i]
-    def multiply(A, l = 1):
-        A = A.matrix
-        B = []
-        for i in range(len(A)):
-            B.append([])
-            for j in range(len(A[0])):
-                B[i].append(l * A[i][j])
-        return B
-    def transpose_rec(A, i=0):
-        A = A.matrix
+    def transpose(A, i=0): 
         j = list(map(lambda x: x[i], A))
         try:
-            return [j] + transpose_rec(A, i+1)
+            return [j] + Matrix.transpose(A, i+1)
         except:
             return [j]
-    def multiplication(A, B, i=0):
+    def scalar(A, B, i=0):
         j = list(map(lambda x, y: x * y, A[i], B[i]))
-        try:
-            return [j] + multiplication(A, B, i+1)
+        try: 
+            return [j] + Matrix.multiplication(A, B, i+1)
         except:
             return [j]
+    def zeros(size, i=0):
+        return [[*[0]*size] for i in range(size)]
+    def eye(size):
+        line = [1] + [*[0]*(size-1)]
+        return [line[-i:] + line[:-i] for i in range(size)]
+    def multiplication(A, B, i=0):
+        C = []
+        for i in A:
+            C_i = []
+            for j in B:
+                C_ij = sum(map(lambda x, y: x * y, i, j))
+                C_i.append(C_ij)
+            C.append(C_i)
+        return C
+    def det(A):
+        if len(A) > 2: 
+            B, C = [line[:0] + line[1:] for line in A], [line[0] for line in A]
+            return sum(x * det(B[:i] + B[i+1:]) * (-1) ** (i) for i, x in enumerate(C))
+        else:
+            return A[0][0] * A[1][1] - A[0][1] * A[1][0]
+    def M(A, i=0, j=0):
+        B = [line[:i] + line[i+1:] for line in A]
+        return B[:j] + B[j+1:]
