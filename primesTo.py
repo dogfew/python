1fc0bff751993b27595aa38eefa4529e1e9ce120
Vladimir Perepelkin <79280805+dogfew@users.noreply.github.com>
2021-07-25 21:18:17 +0300

    Update primesTo.py

--- primesTo.py
+++ primesTo.py
@@ -3,7 +3,7 @@ def primesTo(n):
     a = []
     sqrt = max(x) ** 0.5
     current_prime = 0
-    while len(x) >= 1 and current_prime < sqrt:
+    while current_prime < sqrt:
         current_prime = x[0]
         a.append(x[0])
         x = list(filter(lambda v: v % current_prime != 0, x))
