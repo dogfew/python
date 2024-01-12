645be6af2f85355b458e3a4b0b40af9751980183
Vladimir Perepelkin <79280805+dogfew@users.noreply.github.com>
2024-01-12 04:50:44 +0300

    Update amd.py

--- algorithms/amd.py
+++ algorithms/amd.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from itertools import product, permutations
 
+
 def male_without_match(matches, males):
     for male in males:
         if male not in matches:
@@ -28,7 +29,7 @@ def deferred_acceptance(male_prefs, female_prefs, silent=False):
             matches[male] = male
             continue
         if not silent:
-            print('Trying %s with %s... ' % (male, female), end='')
+            print("Trying %s with %s... " % (male, female), end="")
 
         prev_male = matches.get(female, None)
         if male not in female_prefs[female]:
@@ -38,26 +39,21 @@ def deferred_acceptance(male_prefs, female_prefs, silent=False):
             matches[male] = female
             matches[female] = male
             if not silent:
-                print('auto')
-        elif female_prefs[female].index(male) < \
-             female_prefs[female].index(prev_male):
+                print("auto")
+        elif female_prefs[female].index(male) < female_prefs[female].index(prev_male):
             matches[male] = female
             matches[female] = male
 
             del matches[prev_male]
             if not silent:
-                print('matched')
+                print("matched")
         elif not silent:
-            print('rejected')
+            print("rejected")
     return {male: matches[male] for male in male_prefs.keys()}
 
 
-
-
-
-
 def check_stable(male_prefs, female_prefs, pairs):
-    pairs_woman = {v:k for k, v in pairs.items()}
+    pairs_woman = {v: k for k, v in pairs.items()}
     pairs_man = pairs
     pairs_man.update({k: None for k in set(male_prefs) - set(pairs_man)})
     pairs_woman.update({k: None for k in set(female_prefs) - set(pairs_woman)})
@@ -72,7 +68,9 @@ def check_stable(male_prefs, female_prefs, pairs):
             try:
                 current_female_match = pairs_woman[woman]
                 if current_female_match is not None:
-                    curr_female_match_index = female_prefs[woman].index(current_female_match)
+                    curr_female_match_index = female_prefs[woman].index(
+                        current_female_match
+                    )
                 else:
                     curr_female_match_index = len(female_prefs[woman])
                 potential_female_match_index = female_prefs[woman].index(male)
@@ -81,6 +79,7 @@ def check_stable(male_prefs, female_prefs, pairs):
             except Exception as e:
                 blocking_pairs.append((woman, woman))
     return blocking_pairs
+
 
 def all_possible_combinations(male_prefs, female_prefs, current_match={}):
     males = list(male_prefs.keys())
@@ -97,7 +96,7 @@ def all_possible_combinations(male_prefs, female_prefs, current_match={}):
             current_match[current_male] = female
             possible_matches.extend(
                 all_possible_combinations(male_prefs, female_prefs, current_match)
-                )
+            )
             current_match.pop(current_male)
 
     return possible_matches
@@ -114,21 +113,25 @@ def all_stable_combinations(male_prefs, female_prefs):
 def get_achievable_parners(male_prefs, female_prefs):
     """wrt to male"""
     stable_combinations = all_stable_combinations(male_prefs, female_prefs)
-    return {male: set(stable_combination.get(male, None)
-            for stable_combination in stable_combinations)
-            for male in male_prefs}
+    return {
+        male: set(
+            stable_combination.get(male, None)
+            for stable_combination in stable_combinations
+        )
+        for male in male_prefs
+    }
 
 
 male_prefs = {
     "Joey": ["Rachel", "Monica", "Phoebe"],
     "Ross": ["Rachel", "Phoebe"],
-    "Chandler": ["Monica", "Phoebe", "Rachel"]
+    "Chandler": ["Monica", "Phoebe", "Rachel"],
 }
 
 female_prefs = {
     "Rachel": ["Ross", "Joey", "Chandler"],
     "Monica": ["Chandler", "Joey"],
-    "Phoebe": ["Joey", "Ross", "Chandler"]
+    "Phoebe": ["Joey", "Ross", "Chandler"],
 }
 
 for pairs in [
@@ -136,7 +139,7 @@ for pairs in [
     {"Joey": "Rachel", "Ross": "Monica", "Chandler": "Phoebe"},
     {"Ross": "Rachel", "Joey": "Monica", "Chandler": "Phoebe"},
     deferred_acceptance(male_prefs, female_prefs),
-    {v:k for k, v in deferred_acceptance(female_prefs, male_prefs).items()}
+    {v: k for k, v in deferred_acceptance(female_prefs, male_prefs).items()},
 ]:
     check_stable(male_prefs, female_prefs, pairs)
 
